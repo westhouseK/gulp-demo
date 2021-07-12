16787f09c533ef6ab8ebd3a9724d421d3a6b9c3e
westhouseK <nishiiekazuki@gmail.com>
2021-07-12 22:34:30 +0900

    can compile scss, can create vender prefix

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # gulp-demo
 ```
-git clone
+git clone https://github.com/westhouseK/gulp-demo.git
 npm install
-npm run wat or npx gulp sass
+npx gulp sass
 ```
+
+# できること
+- scssをコンパイルして、cssにする
+- scssの変更を監視する
+- ベンダープレフィックスをつける
+
+# 参考
+- gulpの基本
+https://www.granfairs.com/blog/staff/try-using-gulp
+- gulp v4について
+https://satoyan419.com/post/gulp-v4/
+- ベンダープレフィックスについて
+https://www.i-ryo.com/entry/2020/02/16/110719
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
-var gulp = require("gulp");
-var sass = require("gulp-sass");
+// gulp
+const gulp = require('gulp');
+// Sassのコンパイルに必要なモジュール
+const sass = require('gulp-sass');
+// ベンダープレフィックスに必要のモジュール
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer');
 
-gulp.task("sass", function() {
-    return gulp.watch("sass/*.scss", () => {
-        return gulp.src('sass/*.scss')
-                   .pipe(sass({outputStyle: 'expanded'}))
-                   .pipe(gulp.dest('css'));
-    });
+// タスクを登録
+gulp.task('sass', () => {
+    return gulp.src('./sass/*.scss')
+               // ベンダープレフィックスはpackage.jsonに定義
+               .pipe(postcss([
+                    autoprefixer({
+                        cascade: false
+                    })
+               ]))
+               .pipe(sass({outputStyle: 'expanded'}))
+               .pipe(gulp.dest('./css'));
 });
+
+// 監視時のタスクを定義
+gulp.task('watch', () => {
+    gulp.watch('./sass/*.scss', gulp.task(['sass']));
+});
+
+//「npx gulp」コマンドで実行するタスクを設定 export的な？
+gulp.task('default', gulp.series('watch'), () => {});
